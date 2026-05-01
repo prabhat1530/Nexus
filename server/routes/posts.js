4f2d5626fff7dec5d2d7fc189d18d5061d0ba606
prabhat1530 <152500533+prabhat1530@users.noreply.github.com>
2026-05-01 17:28:10 +0530

    fix: add validation middleware to createPost route

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,7 +4,7 @@ const validate = require('../middleware/validate');
 const upload = require('../middleware/upload');
 const { createPost, getFeed, getExplore, getPost, getUserPosts, updatePost, deletePost, toggleLike, addComment, deleteComment, createPostValidation, commentValidation } = require('../controllers/postController');
 
-router.post('/', auth, upload.single('image'), createPost);
+router.post('/', auth, upload.single('image'), createPostValidation, validate, createPost);
 router.get('/feed', auth, getFeed);
 router.get('/explore', auth, getExplore);
 router.get('/user/:userId', auth, getUserPosts);
